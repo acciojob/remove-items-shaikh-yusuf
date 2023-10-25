f70a45cce5575d705a3e09ace8783f0471b2097d
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-10-25 09:51:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+ function removeEle() {
+        let select = document.getElementById("colorSelect");
+        let selectValue = select.value;
+        console.log(selectValue);
+        let option = select.children;
+        for (let x of option) {
+          if (selectValue === x.value) {
+            x.remove();
+          }
+        }
+      }
